af546d7336c40126f6d54ac2c11884be3e3b6481
Hsuan Lee <hsuan@cobinhood.com>
2018-11-05 15:25:32 +0800

    Deploy version 1.0.0

--- ui/app/components/modals/deposit-ether-modal.js
+++ ui/app/components/modals/deposit-ether-modal.js
@@ -146,7 +146,7 @@ DepositEtherModal.prototype.render = function () {
 
         this.renderRow({
           logo: h('img.deposit-ether-modal__logo', {
-            src: './images/deposit-eth.svg',
+            src: './images/deposit-dexon.svg',
           }),
           title: DIRECT_DEPOSIT_ROW_TITLE,
           text: DIRECT_DEPOSIT_ROW_TEXT,
