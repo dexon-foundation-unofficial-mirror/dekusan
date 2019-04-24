05372982cac9dd95488cbbc627f3b5a7db019159
Hsuan Lee <boczeratul@gmail.com>
2019-04-24 16:22:58 +0800

    Update translations

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 ## Current Develop Branch
 
+## 1.3.3 Wed Apr 24 2019
+
 ## 1.3.2 Fri Apr 19 2019
 
 ## 1.3.1 Wed Apr 17 2019
--- app/scripts/platforms/extension.js
+++ app/scripts/platforms/extension.js
@@ -77,7 +77,7 @@ class ExtensionPlatform {
     const nonce = parseInt(txMeta.txParams.nonce, 16)
 
     const title = 'Confirmed transaction'
-    const message = `Transaction ${nonce} confirmed! View on DEXSCAN`
+    const message = `Transaction ${nonce} confirmed! View on DEXONSCAN`
     this._showNotification(title, message, url)
   }
 
